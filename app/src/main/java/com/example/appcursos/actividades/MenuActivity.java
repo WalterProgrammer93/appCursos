a287ff57a494fba3401d629d121a62993cfb8734
WalterDAW <co2full@outlook.es>
2020-11-30 19:44:43 +0100

    Creando el adaptador MenuAdaptador.java y MenuActivity.java. Ademas de crear los menus action bar e inflarlos en el layout activity_menu.xml

--- app/src/main/java/com/example/appcursos/actividades/MenuActivity.java
+++ app/src/main/java/com/example/appcursos/actividades/MenuActivity.java
@@ -1,16 +1,155 @@
 package com.example.appcursos.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.appcursos.R;
+import com.example.appcursos.adaptadores.MenuAdaptador;
+import com.example.appcursos.bd.UsuarioBD;
 
-public class MenuActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MenuActivity extends AppCompatActivity implements MenuAdaptador.ItemClickListener {
+
+    MenuAdaptador adapter;
+    ArrayList<String> menu;
+    RecyclerView recyclerView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    UsuarioBD ubd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ubd = new UsuarioBD(this);
+        menu = new ArrayList<>();
+        menu.add("Curso");
+        menu.add("Asignaturas");
+        menu.add("Alumnos");
+        menu.add("Profesores");
+        menu.add("Usuarios");
+
+        recyclerView = findViewById(R.id.my_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,LinearLayoutManager.VERTICAL));
+        adapter = new MenuAdaptador(this, menu);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        switch (position) {
+            case 0:
+                Intent i1 = new Intent(getApplicationContext(), CursosActivity.class);
+                startActivity(i1);
+                break;
+            case 1:
+                Intent i2 = new Intent(getApplicationContext(), AlumnosActivity.class);
+                startActivity(i2);
+                break;
+            case 2:
+                Intent i3 = new Intent(getApplicationContext(), AsignaturasActivity.class);
+                startActivity(i3);
+                break;
+            case 3:
+                Intent i4 = new Intent(getApplicationContext(), ProfesoresActivity.class);
+                startActivity(i4);
+                break;
+            default:
+                Toast.makeText(this, "You clicked " + adapter.getItem(position) +
+                        " on row number " + position, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_llamar:
+                llamar();
+                return true;
+            case R.id.action_marcar:
+                marcar();
+                return true;
+            case R.id.action_sms:
+                enviar();
+                return true;
+            case R.id.action_contactos:
+                contactos();
+                return true;
+            case R.id.action_camara:
+                camara();
+                return true;
+            case R.id.action_salir:
+                finish();
+                Toast.makeText(getApplicationContext(), "LogOut Successful", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void llamar() {
+        String numero = "674926136";
+        Intent call = new Intent(Intent.ACTION_CALL);
+        call.setData(Uri.parse("tel:" + numero));
+        if (call.resolveActivity(getPackageManager()) != null) {
+            startActivity(call);
+        }
+    }
+
+    public void marcar() {
+        Intent dial = new Intent(Intent.ACTION_DIAL);
+        if (dial.resolveActivity(getPackageManager()) != null) {
+            startActivity(dial);
+        }
+    }
+
+    public void enviar() {
+        String strPhone = "640608295";
+        String strMessage = "LoremnIpsum";
+        Uri sms_uri = Uri.parse("smsto:+" + strPhone);
+        Intent sms = new Intent(Intent.ACTION_SENDTO, sms_uri);
+        sms.putExtra("sms_body", strMessage);
+        startActivity(sms);
+    }
+
+    public void contactos() {
+        Intent contactos = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        if (contactos.resolveActivity(getPackageManager()) != null) {
+            startActivity(contactos);
+        }
+    }
+
+    public void camara() {
+        //String targetFilename = "captura";
+        Intent camara = new Intent("android.media.action.IMAGE_CAPTURE");
+        /*camara.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.withAppendedPath(locationForPhotos, targetFilename));*/
+        if (camara.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(camara, REQUEST_IMAGE_CAPTURE);
+        }
+
     }
 }
--- app/src/main/java/com/example/appcursos/adaptadores/MenuAdaptador.java
+++ app/src/main/java/com/example/appcursos/adaptadores/MenuAdaptador.java
@@ -1,4 +1,71 @@
 package com.example.appcursos.adaptadores;
 
-public class MenuAdaptador {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.appcursos.R;
+import java.util.List;
+
+public class MenuAdaptador extends RecyclerView.Adapter<MenuAdaptador.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    public MenuAdaptador(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public MenuAdaptador.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.activity_main, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MenuAdaptador.ViewHolder holder, int position) {
+        String menu = mData.get(position);
+        holder.opcion.setText(menu);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView opcion;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            opcion = itemView.findViewById(R.id.opcion);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    public String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
 }
