8256b68196e5da934162b7937ccb433b807c927c
WalterDAW <co2full@outlook.es>
2020-12-01 02:13:16 +0100

    Creacion de la actividad NuevoCursoActivity.java y activity_nuevo_curso.java Modificacion del AndroidManifest.xml, CursosActivity.java y los menus menu1.xml y menu_contextual.xml

--- app/src/main/java/com/example/appcursos/actividades/CursosActivity.java
+++ app/src/main/java/com/example/appcursos/actividades/CursosActivity.java
@@ -1,9 +1,18 @@
 package com.example.appcursos.actividades;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.appcursos.R;
 import com.example.appcursos.adaptadores.CursoAdaptador;
@@ -26,6 +35,57 @@ public class CursosActivity extends AppCompatActivity {
         cursoAdaptador = new CursoAdaptador(this, listaCursos);
         lvCursos = findViewById(R.id.lvCursos);
         lvCursos.setAdapter(cursoAdaptador);
+        registerForContextMenu(lvCursos);
         cursoAdaptador.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_nuevo:
+                Intent nuevo_curso = new Intent(CursosActivity.this, NuevoCursoActivity.class);
+                startActivity(nuevo_curso);
+                return true;
+            case R.id.action_salir:
+                finish();
+                Toast.makeText(getApplicationContext(), "LogOut Successful", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_contextual, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final int itemSeleccionado = info.position;
+
+        switch (item.getItemId()) {
+            case R.id.action_editar:
+                // hacer algo
+                return true;
+            case R.id.action_eliminar:
+                // hacer algo
+                return true;
+            case R.id.action_detalles:
+                // hacer algo
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
