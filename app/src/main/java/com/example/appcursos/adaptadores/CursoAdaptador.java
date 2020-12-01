18276df952756ca0d0f883ec4a42b499cdf766a6
WalterDAW <co2full@outlook.es>
2020-12-01 02:43:28 +0100

    Creacion de las actividades: - NuevoAsignaturaActivity.java - NuevoAlumnoActivity.java - NuevoProfesorActivity.java
    Creacion de los layouts:
    - activity_nuevo_asignatura.xml
    - activity_nuevo_alumno.xml
    - activity_nuevo_profesor.xml
    
    Modificacion de los ficheros AndroidManifest.xml y CursoAdaptador.java, ademas de los ficheros strings.xml

--- app/src/main/java/com/example/appcursos/adaptadores/CursoAdaptador.java
+++ app/src/main/java/com/example/appcursos/adaptadores/CursoAdaptador.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.example.appcursos.R;
 import com.example.appcursos.modelos.Curso;
 import java.util.ArrayList;
