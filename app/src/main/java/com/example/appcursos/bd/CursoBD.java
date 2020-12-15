1b56304118f5c52f5e33d00bb54f82d218572f5c
WalterDAW <co2full@outlook.es>
2020-12-15 17:58:55 +0100

    Modificado el fichero CursoBD.java

--- app/src/main/java/com/example/appcursos/bd/CursoBD.java
+++ app/src/main/java/com/example/appcursos/bd/CursoBD.java
@@ -33,6 +33,20 @@ public class CursoBD {
         abd = new AdminBD(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    public void leerBD() {
+        bd = abd.getReadableDatabase();
+    }
+    public void escribirBD() {
+        bd = abd.getWritableDatabase();
+    }
+    public void cerrarBD() {
+        bd.close();
+    }
+
+    public SQLiteDatabase getBD() {
+        return bd;
+    }
+
     public void insertarCurso(Curso curso) {
         bd = abd.getWritableDatabase();
         ContentValues registro = new ContentValues();
