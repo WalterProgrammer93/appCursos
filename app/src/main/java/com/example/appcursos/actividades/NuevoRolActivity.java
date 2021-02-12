4f9dcecf38d328dba5b008978d7b9fc6638b195b
WalterDAW <co2full@outlook.es>
2021-02-12 14:45:04 +0100

    Creado las actividades de rol y permisos

---
+++ app/src/main/java/com/example/appcursos/actividades/NuevoRolActivity.java
@@ -0,0 +1,16 @@
+package com.example.appcursos.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appcursos.R;
+
+public class NuevoRolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_rol);
+    }
+}
