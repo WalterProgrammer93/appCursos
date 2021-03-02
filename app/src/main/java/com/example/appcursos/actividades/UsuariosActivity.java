f99e7ae6931b44444bbb17b702352a72a69e2455
WalterDAW <co2full@outlook.es>
2021-03-02 19:27:47 +0100

    Modificando los menus contextuales de cada item de todoas las opciones del menu y dialogos

--- app/src/main/java/com/example/appcursos/actividades/UsuariosActivity.java
+++ app/src/main/java/com/example/appcursos/actividades/UsuariosActivity.java
@@ -1,9 +1,12 @@
 package com.example.appcursos.actividades;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -84,18 +87,30 @@ public class UsuariosActivity extends AppCompatActivity {
                 (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         final int itemSeleccionado = info.position;
 
-        switch (itemSeleccionado) {
-            case R.id.action_editar:
-                // hacer algo
-                return true;
-            case R.id.action_eliminar:
-                // hacer algo
-                return true;
-            case R.id.action_buscar:
-                // hacer algo
-                return true;
-            default:
-                return super.onContextItemSelected(item);
+        if (itemSeleccionado == R.id.action_editar) {// hacer algo
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.lb_esta_seguro)
+                    .setPositiveButton(R.string.lb_si,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // Qué hacer si el usuario pulsa "Si"
+                                    Intent editar = new Intent(UsuariosActivity.this, NuevoUsuarioActivity.class);
+                                    startActivity(editar);
+                                }
+                            })
+                    .setNegativeButton(R.string.lb_no,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // Qué hacer si el usuario pulsa "No"
+                                    // En este caso se cierra directamente el diálogo y no se hace nada más
+                                    dialog.dismiss();
+                                }
+                            });
+            builder.create().show();
+            return true;
         }
+        return super.onContextItemSelected(item);
     }
 }
